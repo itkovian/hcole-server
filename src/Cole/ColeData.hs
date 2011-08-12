cb028f0171ca7a9570238534c736015b148894dd
Andy Georges <andy.georges@elis.ugent.be>
2011-08-12 18:29:14 +0200

    Changed ColeData to use Text.CSV.ByteString rather than raw ByteStrings for representing the data. JSONifying is done automagically.

--- src/Cole/ColeData.hs
+++ src/Cole/ColeData.hs
@@ -12,9 +12,14 @@ module Cole.ColeData
   , mkColeData
   ) where
 
+import           Control.Monad (join, liftM)
 import qualified Data.Aeson as A
-import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC (lines, unlines)
+import           Data.ByteString.Internal (isSpaceWord8)
+import qualified Data.ByteString.Lazy as BSL
 import           Data.Text (pack)
+import qualified Text.CSV.ByteString as CSVB
 
 import Cole.Files.Cache
 
@@ -22,25 +27,31 @@ import Cole.Files.Cache
 -- | Data type for the COLE information. FIXME: should also parse the 
 -- ByteString and get real data out of it.
 data ColeData = ColeData
-  { coleRefSpeedup       :: Maybe BS.ByteString
-  , coleTrainSpeedup     :: Maybe BS.ByteString
-  , coleCompilationTime  :: Maybe BS.ByteString
-  , coleCodeSize         :: Maybe BS.ByteString
-  , coleRefEnergyUsage   :: Maybe BS.ByteString
-  , coleTrainEnergyUsage :: Maybe BS.ByteString
+  { coleRefSpeedup       :: Maybe CSVB.CSV
+  , coleTrainSpeedup     :: Maybe CSVB.CSV
+  , coleCompilationTime  :: Maybe CSVB.CSV
+  , coleCodeSize         :: Maybe CSVB.CSV
+  , coleRefEnergyUsage   :: Maybe CSVB.CSV
+  , coleTrainEnergyUsage :: Maybe CSVB.CSV
   }
 
 
 mkColeData :: FilePath -> IO ColeData
 mkColeData fp = do 
   cacheFile <- mkCacheFile fp
-  return $ ColeData { coleRefSpeedup       = getRefSpeedup cacheFile
-                    , coleTrainSpeedup     = getTrainSpeedup cacheFile
-                    , coleCompilationTime  = getCompilationTime cacheFile
-                    , coleCodeSize         = getCodeSize cacheFile
-                    , coleRefEnergyUsage   = getRefEnergyUsage cacheFile
-                    , coleTrainEnergyUsage = getTrainEnergyUsage cacheFile
+  return $ ColeData { coleRefSpeedup       = join $ liftM convertToCSV $ getRefSpeedup cacheFile
+                    , coleTrainSpeedup     = join $ liftM convertToCSV $ getTrainSpeedup cacheFile
+                    , coleCompilationTime  = join $ liftM convertToCSV $ getCompilationTime cacheFile
+                    , coleCodeSize         = join $ liftM convertToCSV $ getCodeSize cacheFile
+                    , coleRefEnergyUsage   = join $ liftM convertToCSV $ getRefEnergyUsage cacheFile
+                    , coleTrainEnergyUsage = join $ liftM convertToCSV $ getTrainEnergyUsage cacheFile
                     }
+    where convertToCSV = CSVB.parseCSV                        -- Read into a CSV structure
+                       . BSC.unlines                          -- Back to a single ByteString
+                       . map (BS.filter (not . isSpaceWord8)) -- Fixoring @boegels stupid format :-)
+                       . BSC.lines                            -- Split so we do not remove newlines ^  
+                       . BS.concat                            -- String them together (they are smaller than 64KiB
+                       . BSL.toChunks                         -- Make it strict.
 
 instance A.ToJSON ColeData where
   toJSON cd = A.object [ "coleRefSpeedup"       A..= coleRefSpeedup cd
