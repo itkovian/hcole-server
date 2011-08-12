2f339eae9f41fd36803d48e9d35ee7f405e9b0b8
Andy Georges <andy.georges@elis.ugent.be>
2011-08-12 15:27:28 +0200

    Made ColeData an instance of Aeson.ToJSON.

--- src/Cole/ColeData.hs
+++ src/Cole/ColeData.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-
  
 Datatype to abstract away the data stored in the COLE files. We
@@ -11,7 +12,9 @@ module Cole.ColeData
   , mkColeData
   ) where
 
+import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as BS
+import           Data.Text (pack)
 
 import Cole.Files.Cache
 
@@ -39,7 +42,15 @@ mkColeData fp = do
                     , coleTrainEnergyUsage = getTrainEnergyUsage cacheFile
                     }
 
-  
+instance A.ToJSON ColeData where
+  toJSON cd = A.object [ pack "coleRefSpeedup"       A..= coleRefSpeedup cd
+                       , pack "coleTrainSpeedup"     A..= coleTrainSpeedup cd
+                       , pack "coleCompilationTime"  A..= coleCompilationTime cd
+                       , pack "coleCodeSize"         A..= coleCodeSize cd
+                       , pack "coleRefEnergyUsage"   A..= coleRefEnergyUsage cd
+                       , pack "coleTrainEnergyUsage" A..= coleTrainEnergyUsage cd
+                       ]
 
+-- FIXME: Perhaps we should also have a FromJSON instance?
 
 
